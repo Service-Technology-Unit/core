14be95d01e81e2d68a0eda1583f3db22793fc88c
Jeff Chilton <jrchilton@ucdavis.edu>
2020-03-17 09:32:58 -0700

    v1.0.2

--- core/src/main/java/edu/ucdavis/ucdh/stu/core/utils/HttpClientProvider.java
+++ core/src/main/java/edu/ucdavis/ucdh/stu/core/utils/HttpClientProvider.java
@@ -3,23 +3,41 @@ package edu.ucdavis.ucdh.stu.core.utils;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContextBuilder;
 
 public class HttpClientProvider {
 	private static final HttpClientProvider provider = new HttpClientProvider();
 	private HttpClient client = null;
+	private SSLContextBuilder contextBuilder = null;
+	private SSLConnectionSocketFactory socketFactory = null;
 
 	/**
 	 * <p>Primary constructor</p>
+	 * 
 	 */
 	public HttpClientProvider() {
 		super();
+		try {
+			this.contextBuilder = SSLContextBuilder.create().loadTrustMaterial(null, new TrustStrategy() {
+				@Override
+				public boolean isTrusted(X509Certificate[] chain, String authType)
+						throws CertificateException {
+					return true;
+				}
+			});
+			this.socketFactory = new SSLConnectionSocketFactory(contextBuilder.build(), new NoopHostnameVerifier());
+		} catch (Exception e) {
+			// oops!
+		}
 	}
 
 	public static HttpClient getClient() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
@@ -30,11 +48,11 @@ public class HttpClientProvider {
 		if (client == null) {
 			client = buildHttpClient();
 		}
-		return client;
+//		return client;
+		return buildHttpClient();
 	}
 
 	private HttpClient buildHttpClient() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-        SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(SSLContextBuilder.create().loadTrustMaterial(new TrustSelfSignedStrategy()).build(), new NoopHostnameVerifier());
-        return HttpClients.custom().setSSLSocketFactory(connectionFactory).build();
+        return HttpClients.custom().setSSLSocketFactory(socketFactory).setConnectionManager(new PoolingHttpClientConnectionManager()).build();
 	}
 }
